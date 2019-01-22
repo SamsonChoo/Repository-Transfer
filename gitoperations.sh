82df12f815351e439f323955396869bcf8b89c9f
SamsonChoo <hanye_choo@mymail.sutd.edu.sg>
2019-01-22 21:06:07 +0800

    corrected wrong folder and username

--- gitoperations.sh
+++ gitoperations.sh
@@ -1,29 +1,18 @@
 chmod +x .
 for i in 123
 do
-  curl -u $1 https://api.github.com/user/repos -d "{\"name\":\"nctuLab$i\"}"
-done
+curl -u $1 https://api.github.com/user/repos -d "{\"name\":\"nctuLab$i\"}"
 mkdir temp
 cd temp
-git clone https://github.com/nctucn/lab1-SamsonChoo
-cd lab1-SamsonChoo
-git remote set-url origin https://github.com/SamsonChoo/nctuLab1
+lab = "https://github.com/nctucn/lab$i-$1"
+git clone $lab
+folder = "lab$i-$1"
+cd $folder
+git remote set-url origin https://github.com/$1/nctuLab$i
 git add .
 git commit -am "transferred my Networks lab from private repository"
 git push
 cd ..
-git clone https://github.com/nctucn/lab2-SamsonChoo
-cd lab2-SamsonChoo
-git remote set-url origin https://github.com/SamsonChoo/nctuLab2
-git add .
-git commit -am "transferred my Networks lab from private repository"
-git push
+done
 cd ..
-git clone https://github.com/nctucn/lab3-SamsonChoo
-cd lab3-SamsonChoo
-git remote set-url origin https://github.com/SamsonChoo/nctuLab3
-git add .
-git commit -am "transferred my Networks lab from private repository"
-git push
-cd ../..
 rm -rf temp
