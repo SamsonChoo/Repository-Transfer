e1bf49d3bd9e05da44996ea6bdcbc2c7ccf5e421
SamsonChoo <hanye_choo@mymail.sutd.edu.sg>
2019-01-23 16:05:18 +0800

    corrected temp folder

--- gitoperations.sh
+++ gitoperations.sh
@@ -1,9 +1,9 @@
 chmod +x .
+mkdir temp
+cd temp
 for i in 1 2 3
 do
 curl -u $1 https://api.github.com/user/repos -d "{\"name\":\"nctuLab$i\"}"
-mkdir temp
-cd temp
 lab="https://github.com/nctucn/lab$i-$1"
 git clone $lab
 folder="lab$i-$1"
